4432b62311d413a9516d34c88a98790dbbe051c3
Ritou19 <steven.habegger@IG13-HABEGGERS.intra.eiaj.ch>
2015-06-30 10:03:29 +0200

    Version_5.1
    Déplacement des méthode dans un service pour ne pas avoir les procédure
    à double

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,37 +8,34 @@
  * Controller of the movieDemoApp
  */
 angular.module('movieDemoApp')
-  .controller('MainCtrl', function ($scope) {
-  	var listFilmJSON = localStorage.getItem('listFilm');
-  	$scope.listFilm = JSON.parse(listFilmJSON);
+  .controller('MainCtrl', function ($scope, MoviesDB) {
+
+  	$scope.listFilm = MoviesDB.getMovies();
 
 //	Incrémenter la liste par des nombres	
 //	for(var i=0; i<100; i++) {
 //	 $scope.awesomeThings.push('Titre '+ (i+1));
 //	};
 	
+	$scope.MoviesDB = MoviesDB;
     $scope.nbAffiche=10;
 	$scope.debut=0;
+	$scope.active = true;
 	
 	$scope.monClick = function(){
-	
-		$scope.listFilm.push({'titre' : $scope.titreFilm, 'description': $scope.descriptionFilm});
-		$scope.serealizer($scope.listFilm);
+		
+		MoviesDB.addFilm($scope.titreFilm, $scope.descriptionFilm);
 		$scope.titreFilm = '';
 		$scope.descriptionFilm = '';
 	
 	}; 
 	
-	$scope.active = true;
+	
 	
 	$scope.supprimerFilm = function(film) {
-		var removed = $scope.listFilm.splice($scope.listFilm.indexOf(film), 1);
-		$scope.serealizer($scope.listFilm);
-	}
+		MoviesDB.remove(film);
+	};
 
-	 $scope.serealizer = function (listFilm) {
-		var jsonFilm = JSON.stringify(listFilm);
-		localStorage.setItem('listFilm', jsonFilm);
-	 }
+	 
 
   });
--- app/scripts/services/moviesdb.js
+++ app/scripts/services/moviesdb.js
@@ -14,15 +14,26 @@ angular.module('movieDemoApp')
 
     var listFilm = [];
 
-   
-	
+    function serealizer (listFilm) {
+    	var jsonFilm = JSON.stringify(listFilm);
+		localStorage.setItem('listFilm', jsonFilm);
+	 };
 
 	
-
     // Public API here
     return {
       getMovies: function() {
+      	var listFilmJSON = localStorage.getItem('listFilm');
+	 	listFilm = JSON.parse(listFilmJSON);
         return listFilm;
-      }
+      },
+      addFilm: function (titreFilm, descriptionFilm){
+      	listFilm.push({'titre' : titreFilm, 'description': descriptionFilm});
+		serealizer(listFilm);
+	  },
+	  remove: function(film){
+	  	var removed = listFilm.splice(listFilm.indexOf(film), 1);
+		serealizer(listFilm);
+		}
     };
   });
